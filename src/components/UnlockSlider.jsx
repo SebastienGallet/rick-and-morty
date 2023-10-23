3fad1f14c7f47c19cccb2039c12749c7c38d3755
Sebastien Gallet <sgallet.dev@gmail.com>
2023-10-23 16:36:58 +0200

    loader phone ready

--- src/components/UnlockSlider.jsx
+++ src/components/UnlockSlider.jsx
@@ -3,23 +3,28 @@ import Draggable from 'react-draggable';
 import './UnlockSlider.css';
 
 const UnlockSlider = ({ onUnlock }) => {
-
   const handleStop = (event, data) => {
-    if (data.x >= 250) { 
+    if (data.x >= 250) {
       onUnlock();
     }
   };
 
+  const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  const currentDate = new Date().toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+
   return (
-    <div className="slider-container">
-      <div className="slider-track">
-        <Draggable 
-          axis="x"
-          bounds="parent"
-          onStop={handleStop}
-        >
-          <div className="slider-thumb" />
-        </Draggable>
+    <div className="iphone-container">
+    <div className='notch'></div>
+      <div className='localTime'>
+        <div className="time">{currentTime}</div>
+        <div className="date">{currentDate}</div>
+      </div>
+      <div className="slider-container">
+        <div className="slider-track">
+          <Draggable axis="x" bounds="parent" onStop={handleStop}>
+            <div className="slider-thumb" />
+          </Draggable>
+        </div>
       </div>
     </div>
   );
