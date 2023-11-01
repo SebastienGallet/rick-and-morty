cea8005ebbe6b286518ddc0ee7bbc5fa8a070a28
Sebastien Gallet <sgallet.dev@gmail.com>
2023-11-01 20:43:49 +0100

    card background

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
 import CharacterModal from "./CharacterModal";
+import "./CharacterCard.css";
+import redCard from "../assets/images/redcard.webp";
+import greenCard from "../assets/images/greencard.webp";
 
 const CharacterCard = ({ name, image, status, gender, species, origin, location }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  
+  const cardBackground = status === "Alive" ? greenCard : redCard;
 
   return (
-    <div className="card" onClick={() => setIsModalOpen(true)}> 
+    <div className="card" style={{ backgroundImage: `url(${cardBackground})`, backgroundSize: 'cover' }} onClick={() => setIsModalOpen(true)}>
       <img src={image} alt={name} />
       <div className="description">
         <h2 className="name">{name}</h2>
-        <p className="text-gray-700">{status}</p>
+        <div className="aside">
+          <p className="status">{status}</p>
+          <p className="species">{species}</p>
+        </div>
       </div>
-      {isModalOpen && 
+      {/* {isModalOpen && 
         <CharacterModal 
           character={{ name, image, status, gender, species, origin, location }} 
           onClose={() => setIsModalOpen(false)} 
         />
-      }
+      } */}
     </div>
   );
 };
